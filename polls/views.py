2f9fa394370e79e3eb47250ffc3304f96d90c7d3
qinxiaogit <qinxiaolove@gmail.com>
2018-02-06 21:34:44 +0800

    init home

--- polls/views.py
+++ polls/views.py
@@ -29,12 +29,12 @@ def detail(request,question_id=0):
     return render(request,'polls/detail.html',{'question':question})'''
     content = get_object_or_404(Question,pk=question_id)
     return render(request,'polls/detail.html',{'question':content})
+
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
 
 def vote(request,question_id=0):
-    return HttpResponse(question_id);
     p = get_object_or_404(Question,pk=question_id)
     try:
         selected_choice = p.choice_set.get(request.POST['choice'])
