0acd5a71659ea624fa4b8a237ac560a91f814b56
qinxiaogit <qinxiaolove@gmail.com>
2018-02-06 22:08:52 +0800

    通用视图

--- polls/urls.py
+++ polls/urls.py
@@ -13,7 +13,7 @@ from . import views
 #     url(r'^(?P<question_id>[0-9]+)/vote/$',views.vote,name="vote"),
 # ]
 urlpatterns = [
-	url(r'^$',views.indexView.as_view(),name='index'),
+	url(r'^$',views.IndexView.as_view(),name='index'),
 	url(r'^(?P<pk>[0-9]+)/$',views.DetailView.as_view(),name='detail'),
 	url(r'^(?P<pk>[0-9]+)/results/$',views.ResultsView.as_view(),name='results'),
 	 url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
